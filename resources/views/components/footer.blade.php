e3aaf9154c0bc68bfc22df721891a02a03b8f19d
Liam <lsutrera.13@est.ucab.edu.ve>
2024-09-30 15:58:56 -0400

    Creando controladores, y componentes del html

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,65 @@
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-white-50 py-5 px-sm-3 px-lg-5" style="margin-top: 90px;">
+        <div class="row pt-5">
+            <div class="col-lg-3 col-md-6 mb-5">
+                <a title="Logo de hidrobolivar" href="index.html">
+                    <Image
+                        src='/img/logo-hidro.png'
+                        width="110"
+                        heigth="110"
+                        allign="right"
+
+                    />
+                </a>
+                <h6 class="text-white text-uppercase mt-4 mb-3" style="letter-spacing: 5px;">Síguenos</h6>
+                <div class="d-flex justify-content-start">
+                    <a class="btn btn-outline-primary btn-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-outline-primary btn-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-primary btn-square" href="#"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h5 class="text-white text-uppercase mb-4" style="letter-spacing: 5px;">Links</h5>
+                <div class="d-flex flex-column justify-content-start">
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Nosotros</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Noticias</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Acueductos</a>
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Atención al cliente</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h5 class="text-white text-uppercase mb-4" style="letter-spacing: 5px;">Servicios</h5>
+                <div class="d-flex flex-column justify-content-start">
+                    <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Hidrocgs</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+                <h5 class="text-white text-uppercase mb-4" style="letter-spacing: 5px;">Contacto</h5>
+                <p><i class="fa fa-map-marker-alt mr-2"></i>753Q+7JX, Trans. B, Ciudad Guayana 8050, Bolívar, Venezuela.</p>
+                <p><i class="fa fa-phone-alt mr-2"></i>+58 0286-7129800</p>
+                <p><i class="fa fa-envelope mr-2"></i>hidrobolix@example.com</p>
+                <!--<h6 class="text-white text-uppercase mt-4 mb-3" style="letter-spacing: 5px;">Newsletter</h6>
+                <div class="w-100">
+                    <div class="input-group">
+                        <input type="text" class="form-control border-light" style="padding: 25px;" placeholder="Your Email">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary px-3">Sign Up</button>
+                        </div>
+                    </div>
+                </div>-->
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .1) !important;">
+        <div class="row">
+            <div class="col-lg-6 text-center text-md-left mb-3 mb-md-0">
+                <p class="m-0 text-white-50">Copyright &copy; <a href="#">Hidrobolivar</a>. Todos los derechos reservados.</a>
+                </p>
+            </div>
+            <div class="col-lg-6 text-center text-md-right">
+                <p class="m-0 text-white-50">Diseñado por: <a href="#">Gerencia de tecnologia, Hidrobolivar.</a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,43 @@
+    <!-- Navbar Start -->
+    <div class="container-fluid position-relative nav-bar p-0">
+        <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
+            <nav class="navbar navbar-expand-lg bg-light navbar-light shadow-lg py-3 py-lg-0 pl-3 pl-lg-5">
+                <a title="Logo de hidrobolivar" href="index.html">
+                    <Image
+                        src='/img/logo-hidro.png'
+                        width="110"
+                        heigth="110"
+                        allign="right"
+
+                    />
+                </a>
+                <!-- <a href="" class="navbar-brand">
+                    <h1 class="m-0 text-primary"><span class="text-dark">TRAVEL</span>ER</h1>
+                </a> -->
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto py-0">
+                        <a href="nosotros.html" class="nav-item nav-link">Nosotros</a>
+                        <a href="noticias.html" class="nav-item nav-link">Noticias</a>
+                        <a href="acueductos.html" class="nav-item nav-link">Acueductos</a>
+                        <a href="package.html" class="nav-item nav-link">Tour Packages</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu border-0 rounded-0 m-0">
+                                <a href="noticias.html" class="dropdown-item">Noticias</a>
+                                <a href="single.html" class="dropdown-item">Blog Detail</a>
+                                <a href="acueductos.html" class="dropdown-item">Acueductos</a>
+                                <a href="guide.html" class="dropdown-item">Travel Guides</a>
+                                <a href="testimonial.html" class="dropdown-item active">Valores</a>
+                            </div>
+                        </div>
+                        <a href="contacto.html" class="nav-item nav-link">Contacto</a>
+
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->

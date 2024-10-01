a5ff619f728b5a8373290adcb54564781558c32d
Liam <lsutrera.13@est.ucab.edu.ve>
2024-10-01 14:50:52 -0400

    Arreglando el package.json

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -7,6 +7,208 @@
     <title>Nosotros</title>
 </head>
 <body>
+        <!-- About Start -->
+        <div class="container-fluid py-5">
+            <div class="container pt-5">
+                <div class="row">
+                    <div class="col-lg-6" style="min-height: 500px;">
+                        <div class="position-relative h-100">
+                            <img class="position-absolute w-100 h-100" src="img/1.jpg" style="object-fit: cover;">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 pt-5 pb-lg-5">
+                        <div class="about-text bg-white p-4 p-lg-5 my-lg-5 justificacion">
+                            <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Quienes somos</h6>
+                            <h1 class="mb-3">HIDROBOLÍVAR C.A. </h1>
+                            <p>Es una hidrológica del Estado Venezolano creada por el Gobierno Regional y once municipios (Angostura del Orinoco, Caroní, Raúl Leoni, Piar, Sucre, Padre Pedro Chien, Gran Sabana, Cedeño, Roscio, SIfonte y El Callao). Nace para satisfacer y solucionar los problemas del agua en la comunidad, tales como la potabilización, abastecimiento, clasificación y saneamiento del Estado.</p>
+                            <div class="row mb-4">
+                                <div class="col-6">
+                                    <img class="img-fluid" src="img/2.jpg" alt="">
+                                </div>
+                                <div class="col-6">
+                                    <img class="img-fluid" src="img/3.jpg" alt="">
+                                </div>
+                            </div>
+                            <a href="conocenos.html" class="btn btn-primary mt-1">Conócenos</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
 
+
+        <!-- Feature Start -->
+        <div class="container-fluid pb-5">
+            <div class="container pb-5">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="d-flex mb-4 mb-lg-0">
+                            <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-primary mr-3" style="height: 100px; width: 100px;">
+                                <i class="fa fa-2x fa-money-check-alt text-white"></i>
+                            </div>
+                            <div class="d-flex flex-column">
+                                <h5 class="">Paga tus servicios</h5>
+                                <p class="m-0">Evita una multa y mantente al dia.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="d-flex mb-4 mb-lg-0">
+                            <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-primary mr-3" style="height: 100px; width: 100px;">
+                                <i class="fa fa-2x fa-award text-white"></i>
+                            </div>
+                            <div class="d-flex flex-column justificacion">
+                                <h5 class="">Calidad de servicio</h5>
+                                <p class="m-0">Trabajamos con altos estandares de calidad, para ofrecerte el mejor servicio.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="d-flex mb-4 mb-lg-0">
+                            <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-primary mr-3" style="height: 100px; width: 100px;">
+                                <i class="fa fa-2x fa-globe text-white"></i>
+                            </div>
+                            <div class="d-flex flex-column">
+                                <h5 class="">Cobertura total</h5>
+                                <p class="m-0">Estamos en todo el estado Bolivar.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Feature End -->
+
+
+        <!-- Registration Start -->
+        <!-- <div class="container-fluid bg-registration py-5" style="margin: 90px 0;">
+            <div class="container py-5">
+                <div class="row align-items-center">
+                    <div class="col-lg-7 mb-5 mb-lg-0">
+                        <div class="mb-4">
+                            <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">HidroBolívar</h6>
+                            <h1 class="text-white"><span class="text-primary">Hidro</span>Web</h1>
+                        </div>
+                        <p class="text-white">Mi gallo pinto</p>
+                        <ul class="list-inline text-white m-0">
+                            <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Eficiencia</li>
+                            <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Excelencia</li>
+                            <li class="py-2"><i class="fa fa-check text-primary mr-3"></i>Compromiso</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="card border-0">
+                            <div class="card-header bg-primary text-center p-4">
+                                <h1 class="text-white m-0">Inicia sesión</h1>
+                            </div>
+                            <div class="card-body rounded-bottom bg-white p-5">
+                                <form>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control p-4" placeholder="Nombre" required="required" />
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control p-4" placeholder="Email" required="required" />
+                                    </div>
+                                    <div class="form-group">
+                                        <select class="custom-select px-4" style="height: 47px;">
+                                            <option selected>Select a destination</option>
+                                            <option value="1">destination 1</option>
+                                            <option value="2">destination 1</option>
+                                            <option value="3">destination 1</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <button class="btn btn-primary btn-block py-3" type="submit">Inicia ahora</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <!-- Registration End -->
+
+
+        <!-- Team Start -->
+        <div class="container-fluid py-5">
+            <div class="container pt-5 pb-3">
+                <div class="text-center mb-3 pb-3">
+                    <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Hidrobolivar</h6>
+                    <h1>Nuestro organigrama</h1>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                        <div class="team-item bg-white mb-4">
+                            <div class="team-img position-relative overflow-hidden">
+                                <img class="img-fluid w-100" src="img/Maduro.jpg" alt="">
+                                <div class="team-social">
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-twitter"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                            <div class="text-center py-4">
+                                <h5 class="text-truncate">Maduro</h5>
+                                <p class="m-0">Presidente</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                        <div class="team-item bg-white mb-4">
+                            <div class="team-img position-relative overflow-hidden">
+                                <img class="img-fluid w-100" src="img/ministro.png" alt="">
+                                <div class="team-social">
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-twitter"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                            <div class="text-center py-4">
+                                <h5 class="text-truncate">Rodolfo Marco Torres</h5>
+                                <p class="m-0">Presidente Minaguas</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                        <div class="team-item bg-white mb-4">
+                            <div class="team-img position-relative overflow-hidden">
+                                <img class="img-fluid w-100" src="img/gober.jpg" alt="">
+                                <div class="team-social">
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-twitter"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                            <div class="text-center py-4">
+                                <h5 class="text-truncate">Ángel Marcano</h5>
+                                <p class="m-0">Gobernador</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                        <div class="team-item bg-white mb-4">
+                            <div class="team-img position-relative overflow-hidden">
+                                <img class="img-fluid w-100" src="img/team-4.jpg" alt="">
+                                <div class="team-social">
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-twitter"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-facebook-f"></i></a>
+                                    <a class="btn btn-outline-primary btn-square" href=""><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                            <div class="text-center py-4">
+                                <h5 class="text-truncate">Guide Name</h5>
+                                <p class="m-0">Designation</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Team End -->
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hidroweb</title>
-    {{-- @vite('resources/css/app.css') --}}
-</head>
-    <body>
-        <h1>Holis</h1>
-    </body>
-</html>
+@extends('layout')
+
+@section('main')
+    <h1>Jelou</h1>
+@endsection
